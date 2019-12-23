03af408d751056a00d98e15596a174edfd065e40
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-12-23 14:48:47 -0500

    Stretch completed

--- queries.sql
+++ queries.sql
@@ -12,3 +12,11 @@
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
     `SELECT Id, CompanyName, LastName FROM [Order] as o
         JOIN Customer as c ON o.CustomerId = c.Id, Employee as e ON e.Id = o.EmployeeId;`
+
+-- Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 9 records.
+    `SELECT Categoryname, Count(CategoryID) as Count FROM Product as p
+        JOIN Category as c ON p.CategoryId = c.Id
+        Group By CategoryID;`
+-- Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+    `SELECT Distinct OrderID, Count(ProductID) as ItemCount FROM OrderDetail as od
+        Group by OrderId;`
